1bd358b2c3f592cd1e91f6588385affd7e8f21da
Pedro Kelvin Pezarini do Valle <pedro.valle@mobiis.com.br>
2024-12-28 22:37:35 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import logo from './assets/valorantLogo.png';
-import searchIcon from './assets/searchIcon.svg';
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Header from './components/header/Header';
 
 export default function App() {
-	const navigate = useNavigate();
-
-	function backToHome() {
-		navigate('/')
-	}
 
   return (
     <div className='app'>
-      <div className='headerContainer'>
-        <img src={logo} alt="Logo" onClick={(() => {backToHome()})}/>
-        <div className='menuItems'>
-          <p><Link to={'/agents'}>Agents</Link></p>
-          <p><Link to={'/weapons'}>Weapons</Link></p>
-          <p><Link to={'/tiers'}>Tiers</Link></p>
-        </div>
-        <div className="searchcontainer">
-          <div className="inputSearch">
-            <input type="text" placeholder="Search..." />
-            <img src={searchIcon} alt="Search Icon" />
-          </div>
-        </div>
-      </div>
+      <Header />
 	  <Outlet />
     </div>
   );
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,29 @@
+import logo from '../../assets/valorantLogo.png';
+import searchIcon from '../../assets/searchIcon.svg';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import './HeaderContainer.css'
+
+export default function App() {
+	const navigate = useNavigate();
+
+	function backToHome() {
+		navigate('/')
+	}
+
+  return (
+    <div className='headerContainer'>
+        <img src={logo} alt="Logo" onClick={(() => {backToHome()})}/>
+        <div className='menuItems'>
+          <p><Link to={'/agents'}>Agents</Link></p>
+          <p><Link to={'/weapons'}>Weapons</Link></p>
+          <p><Link to={'/tiers'}>Tiers</Link></p>
+        </div>
+        <div className="searchcontainer">
+          <div className="inputSearch">
+            <input type="text" placeholder="Search..." />
+            <img src={searchIcon} alt="Search Icon" />
+          </div>
+        </div>
+    </div>
+  );
+}
